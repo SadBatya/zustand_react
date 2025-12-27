8950fcf808c0317476048c9295a0062106b83411
sadbatya <sadbatya@mail.ru>
2025-12-27 12:57:19 +0200

    feat: added example counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import './App.css'
+import "./App.css";
+import { addTen } from "./helpers/addTen";
+import { useCounterStore } from "./model/countStore";
 
 function App() {
-
+  const { counter, increment, decrement } = useCounterStore();
 
   return (
-    <>
-
-    </>
-  )
+    <div className="wrapper">
+      <button onClick={increment}>+</button>
+      {counter}
+      <button onClick={decrement}>-</button>
+      <button onClick={addTen}>Add 10</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/model/countStore.ts
@@ -0,0 +1,39 @@
+import { create, type StateCreator } from "zustand";
+
+type CounterState = {
+  counter: number;
+};
+
+type CounterActions = {
+  increment: () => void;
+  decrement: () => void;
+  changeByAmount: (value: number) => void;
+};
+
+const counterSlice: StateCreator<CounterState & CounterActions> = (
+  set,
+  get
+) => ({
+  counter: 0,
+  decrement: () => {
+    const { counter } = get();
+    set({ counter: counter - 1 });
+  },
+  increment: () => {
+    const { counter } = get();
+    set({ counter: counter + 1 });
+  },
+  changeByAmount: (value: number) => {
+    const { counter } = get();
+    set({ counter: counter + value });
+  },
+});
+
+export const useCounterStore = create<CounterState & CounterActions>(
+  counterSlice
+);
+
+export const changeByAmount = (value: number) =>
+  useCounterStore.getState().changeByAmount(value);
+export const getCounter = () => useCounterStore.getState().counter;
+export const counter2 = useCounterStore.getState().counter;
