f1dc1f999a498521a857dbffa94cbb25254d34cf
sadbatya <sadbatya@mail.ru>
2026-01-04 13:12:52 +0200

    refactor: удалил неиспользуемые импорты

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,28 +1,12 @@
-import { Dropdown, Button, type MenuProps } from "antd";
+import { Button } from "antd";
 import { CoffeeCategory } from "../model/coffeeTypes";
 import { useCoffeeStore } from "../model/cofeeStore";
-
-const menuItems: MenuProps["items"] = [
-  {
-    label: "Капучино",
-    key: "1",
-  },
-];
-
-const handleMenuClick: MenuProps["onClick"] = (e) => {
-  console.log(e);
-};
-
-const menuProps = {
-  items: menuItems,
-  onClick: handleMenuClick,
-};
 
 export const Category = () => {
   const { params, setParams } = useCoffeeStore();
 
   return (
-    <div menu={menuProps} placement="bottom">
+    <div>
       {Object.keys(CoffeeCategory).map((key) => (
         <Button
           danger={params.type === key}
